88eecf43409f9cc53461dcbefa2ea62e99a6c4c0
ssc1999 <ssc1099.csmm@gmail.com>
2020-11-28 15:08:00 +0100

    Models created in models.py

--- top100/top100_app/models.py
+++ top100/top100_app/models.py
@@ -2,10 +2,44 @@ from django.db import models
 from phone_field import PhoneField
 
 class Contact(models.Model):
-    name = models.CharField(max_length=100)
-    phone = models.CharField(max_length=13)
-    mail = models.CharField(max_length=100)
-    message = models.CharField(max_length=2000)
+    name = models.CharField(max_length = 100)
+    phone = models.CharField(max_length = 13)
+    mail = models.CharField(max_length = 100)
+    message = models.CharField(max_length = 2000)
 
     def __str__(self):
         return self.mail
+
+class Gender(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return "Author: " + self.name
+
+class Song(models.Model):
+    name = models.CharField(max_length=20)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    gender = models.ManyToManyField(Gender)
+    repros = models.IntegerField()
+
+    def __str__(self):
+        return "Song: " + self.name
+
+class Album(models.Model):
+    name = models.CharField(max_length = 20)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    gender = models.ManyToManyField(Gender)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    repros = models.IntegerField()
+
+    def __str__(self):
+        return "Album: " + self.name
+
+
+
